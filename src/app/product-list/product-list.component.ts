022a33f4a2383d3ce004e6d658906ef6186df856
ceflorencio <ceflorencio@gmail.com>
2019-07-05 20:49:57 -0300

    Add files via upload

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 
 import { products } from '../products';
 
+import { Input } from '@angular/core';
+
+import { Output, EventEmitter } from '@angular/core';
+
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  
   products = products;
 
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
 }
 
 
